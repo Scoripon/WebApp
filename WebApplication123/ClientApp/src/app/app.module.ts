d520c0e312e8ba40af2a2bfc99b547a55eeb34b8
Boki <bojanmilicevic91@gmail.com>
2019-03-09 14:39:45 +0100

    Zamena angular aplikacije novijom verzijom - sadasnja verzija je 6.1.10 i dodavanje podrske za SASS

--- WebApplication123/ClientApp/src/app/app.module.ts
+++ WebApplication123/ClientApp/src/app/app.module.ts
@@ -1,40 +1,16 @@
-import { NgModule } from '@angular/core';
-import { UserService } from './services/UserService.service'
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { FetchUserComponent } from './fetchUser/fetchUser.component';
-import { createUser } from './addUser/createUser.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    FetchUserComponent,
-    createUser,
+    AppComponent
   ],
   imports: [
-    CommonModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'fetch-user', component: FetchUserComponent },
-      { path: 'register-user', component: createUser },
-      { path: 'user/edit/:id_korisnik', component: createUser },
-      { path: '**', redirectTo: 'home' }
-    ])
+    BrowserModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
